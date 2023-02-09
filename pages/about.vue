0fef22f66f912fc330b8364496184b20031baed8
markov <markovpavel32@gmail.com>
2023-02-09 16:39:50 +0300

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>11112<h1>Example heading <span class="badge bg-secondary">New</span></h1></div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped></style>
