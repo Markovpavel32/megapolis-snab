331ff1e6b450c2749c2da340caf5ecbd976a0b9b
markov <markovpavel32@gmail.com>
2023-04-09 18:27:14 +0300

    eslint config changes

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:nuxt/recommended",
+    "plugin:vue/vue3-recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": {
+        "max": 3
+      },
+      "multiline": {
+        "max": 1
+      }
+    }],
+    "semi": ["error", "never"],
+    "vue/multi-word-component-names": ["error", {
+      "ignores": ["index"]
+    }]
+  },
+};
