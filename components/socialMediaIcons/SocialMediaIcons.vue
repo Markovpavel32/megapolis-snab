d9a45b0e1b7485c3e70b997c642a56870c1721f6
markov <markovpavel32@gmail.com>
2023-04-09 18:32:48 +0300

    default layout and social media icons

---
+++ components/socialMediaIcons/SocialMediaIcons.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="social-media-icons">
+    <telegram class="social-media-icons__icon clickable" />
+    <whatsapp class="social-media-icons__icon clickable" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Telegram from '~/assets/icons/telegram.svg'
+import Whatsapp from '~/assets/icons/whatsapp.svg'
+</script>
+
+<style lang="scss" scoped>
+.social-media-icons {
+  display: flex;
+  gap: 8px;
+  padding: 8px;
+
+  &__icon {
+    flex-basis: 32px;
+    width: 32px;
+    height: 32px;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="layout">
-     <app-header />
+    <app-header />
     <slot />
   </div>
 </template>
 
 <script setup lang="ts">
-import AppHeader from '~/components/appHeader/AppHeader.vue';
+import AppHeader from '~/components/appHeader/AppHeader.vue'
 </script>
 
 <style lang="scss" scoped></style>
