4eca95580d0387d7201b4796d598681becc36d33
markov <markovpavel32@gmail.com>
2023-07-10 15:47:41 +0300

    add env add forms api to send mails

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import svgLoader from "vite-svg-loader"
+
 export default defineNuxtConfig({
   modules: ['@bootstrap-vue-next/nuxt'],
   css: ['~/assets/styles/main.scss', 'vue3-carousel/dist/carousel.css'],
   vite: {
     plugins: [svgLoader()]
+  },
+  nitro: {
+    plugins: ['~/server/index.ts'],
+    // storage: {
+    //   db: {
+    //
+    //   }
+    // }
+  },
+  runtimeConfig: {
+    mailer: {
+      host: process.env.MAILER_HOST,
+      port: process.env.MAILER_PORT,
+      auth: {
+        user: process.env.MAILER_USER,
+        pass: process.env.MAILER_PASS,
+      }
+    }
   }
 })
